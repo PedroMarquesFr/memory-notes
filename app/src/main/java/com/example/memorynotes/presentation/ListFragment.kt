a611fe22d7f30b856bf3d7676747695e3d135f1a
Your Name <you@example.com>
2022-07-05 18:38:14 -0300

    update ListFragment

--- app/src/main/java/com/example/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/example/memorynotes/presentation/ListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.example.memorynotes.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -36,6 +38,15 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action: NavDirections = ListFragmentDirections.actionGoToNote(id)
+        //Navigation.findNavController().navigate(action)
     }
 
     companion object {
